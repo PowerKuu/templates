e62fc6cd9c0ae3e6dc9744dfad109cdad48edeed
Håkon Kleven <hkleven6@outlook.com>
2023-03-25 18:25:16 +0100

    Update router.ts

--- vue/src/router.ts
+++ vue/src/router.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router"
-import HomeView from "@/views/Home.vue"
+import Home from "@/views/Home.vue"
 
 export default createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +7,7 @@ export default createRouter({
     {
       path: "/",
       name: "home",
-      component: HomeView
+      component: Home
     }
   ]
 })
